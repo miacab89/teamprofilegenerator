8049ae20792b76a219b1f1e6d24aaadcd4fd2b78
Mia Cabrera <58441831+miacab89@users.noreply.github.com>
2020-02-29 12:45:05 -0600

    Create managers.js

---
+++ managers.js
@@ -0,0 +1,39 @@
+ class Person {
+        constructor (fullName, id, email, office ) {
+            this.fullName = fullName; 
+            this.id = id;
+            this.email = email;
+            this.office = office;
+
+
+        }
+
+    }
+
+    class Managers extends Person {
+        constructor (fullName )
+        {
+            super(fullName);
+            this.id = id;
+            this.email = email;
+            this.office = office;
+            
+
+            
+        }
+    }
+
+ 
+// const getManagers = []; 
+
+//     getManagers(); {
+//         console.log("Managers Name:" + managersFullName)
+//         //return this.fullName + " " + lastName; 
+        
+//     }
+
+    // getRole () {
+
+    // }
+
+    node_modules.Managers; 

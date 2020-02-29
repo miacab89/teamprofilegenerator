e4597396fab8da5f9f5e1159e2651c2c1c55b281
Mia Cabrera <58441831+miacab89@users.noreply.github.com>
2020-02-29 12:44:30 -0600

    Create intern.js

---
+++ intern.js
@@ -0,0 +1,36 @@
+class Person {
+        constructor (fullName, id, email, school) {
+            this.fullName = fullName;
+            this.id = id;
+            this.email = email;
+            this.school = school;
+
+
+        }
+
+    }
+
+    class Intern extends Person {
+        constructor (fullName, id, email, school)
+        {
+            super(fullName);
+            this.id = id;
+            this.email = email;
+            this.school = school;
+
+            
+        }
+    }
+
+   
+
+   // getInterns(); {
+        //return this.fullName + " " + lastName; 
+        
+    
+
+    // getRole () {
+
+    // }
+
+    node_modules.Intern; 

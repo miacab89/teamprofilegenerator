050e62fc682bbaf2b1a4395c106dbd4c00dc02f4
Mia Cabrera <58441831+miacab89@users.noreply.github.com>
2020-02-29 12:43:50 -0600

    Create engineers.js

---
+++ engineers.js
@@ -0,0 +1,33 @@
+class Person {
+        constructor (fullName, id, email, github ) {
+            this.fullName = fullName;
+            this.id = id;
+            this.email = email;
+            this.github = github;
+
+
+        }
+
+    getFullName() {
+        return this.FullName + " "; 
+    }        
+
+    getid() {
+        return this.id + " "; 
+    }
+
+    }
+
+    class Engineers extends Person {
+        constructor (fullName )
+        {
+            super(fullName);
+            this.id = id;
+            this.email = email;
+            this.github = github;
+
+            
+        }
+    }
+
+  node_modules.Engineers; 
